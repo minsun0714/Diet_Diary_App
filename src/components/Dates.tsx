fee8ce4efae2c195b3e3ea44191e4a304fd6b5ec
jasmine0714 <woooooooow22@gmail.com>
2023-04-22 17:55:53 +0900

    ✨ new Date() 사용하여 이번 달 날짜가 담긴 2차원 배열을 구함

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -2,6 +2,34 @@ import React from "react";
 import styled from "styled-components";
 
 function Dates() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+
+  // 이번 달 1일이 무슨 요일인지 구함
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+  // 이번 달이 며칠까지 있는지 구함
+  const lastDateOfMonth = new Date(year, month + 1, 0).getDate();
+
+  // 날짜를 채워줄 2차원 빈 배열 선언
+  let daysOfThisMonth = [...Array(6)].map((_) => [...Array(7)].map(() => 0));
+  let day = firstDayOfMonth;
+  // 이번 달 1일에 해당하는 원소에 1을 할당
+  daysOfThisMonth[0][day] = 1;
+
+  for (let i = 0; i < daysOfThisMonth.length; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (daysOfThisMonth[i][j - 1]) {
+        daysOfThisMonth[i][j] = daysOfThisMonth[i][j - 1] + 1;
+      } else if (daysOfThisMonth[i - 1]?.[6]) {
+        daysOfThisMonth[i][j] = daysOfThisMonth[i - 1][6] + 1;
+      }
+      if (daysOfThisMonth[i][j] === lastDateOfMonth) break;
+    }
+  }
+
+  console.log(daysOfThisMonth);
+
   return null;
 }
 export default Dates;
