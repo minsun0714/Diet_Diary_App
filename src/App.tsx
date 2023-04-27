616ee9752c2d3b367bf3f7b3c4d02ef13c183e5d
jasmine0714 <woooooooow22@gmail.com>
2023-04-27 19:44:54 +0900

    💄 media-query 적용(max-width: 500px)

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,28 @@ const Table = styled.table`
   align-items: center;
   border: 1px solid transparent;
   border-radius: 20px;
-  height: 85vh;
+  height: 850px;
   width: 70vw;
   margin: auto;
   background: rgba(0, 0, 0, 0.2);
   box-shadow: 2px 2px 4px #999;
+
+  @media (max-width: 500px) {
+    display: none;
+  }
 `;
 
 const Year = styled.caption`
   position: absolute;
   font-size: 50px;
-  margin-top: -600px;
+  margin-bottom: 700px;
   color: gray;
   text-shadow: 2px 2px 4px white;
 `;
 
 const Month = styled.caption`
   position: absolute;
-  margin-bottom: 450px;
+  margin-bottom: 500px;
   font-size: 70px;
   color: white;
   text-shadow: 2px 2px 4px pink;
