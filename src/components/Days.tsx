403f1e0448cf52fd41e0a151e4a7c5d7e3d14007
jasmine0714 <woooooooow22@gmail.com>
2023-04-29 14:42:40 +0900

    🎨 DaysContainer HTML 태그 tbody에서 thead로 변경

--- src/components/Days.tsx
+++ src/components/Days.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const DaysContainer = styled.tbody`
-  position: absolute;
-  margin-bottom: 290px;
-`;
 const Day = styled.th`
   font-size: 40px;
   width: 9.8vw;
   color: white;
   text-shadow: 2px 2px 0.5px rgba(0, 0, 0, 0.5);
+`;
+
+const DaysContainer = styled.thead`
+  position: absolute;
+  margin-bottom: 290px;
 `;
 function Days() {
   const days = ["일", "월", "화", "수", "목", "금", "토"];
