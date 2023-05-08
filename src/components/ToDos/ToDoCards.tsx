b4b4a38f6375d8dd1b02cb182c4904f88f076d54
jasmine0714 <woooooooow22@gmail.com>
2023-05-08 11:52:42 +0900

    🎨 textarea에 있는 memo를 memoList에 추가하도록 구현

--- src/components/ToDos/ToDoCards.tsx
+++ src/components/ToDos/ToDoCards.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function ToDoCards() {
+  const [memoList, setMemoList] = useState<string[]>([]);
+  const [memo, setMemo] = useState("");
+
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(event.target.value);
+    setMemo(event.target.value);
+  };
+
+  const onClick = () => {
+    setMemoList([memo, ...memoList]);
+    console.log(memoList);
+  };
   return (
     <div>
-      <ul></ul>
+      <textarea onChange={onChange}></textarea>
+      <button onClick={onClick}>+</button>
+      <ul>
+        {memoList.map((memo: string, idx: number) => (
+          <li key={idx}>{memo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
