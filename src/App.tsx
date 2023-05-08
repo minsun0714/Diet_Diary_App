bad20f69af632b0fe8a0c516f9cc5766d36e86ad
jasmine0714 <woooooooow22@gmail.com>
2023-05-08 11:51:38 +0900

    🎨 Main styled-component 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import styled from "styled-components";
 import Calendar from "./components/Calendar/Table";
 import ToDoCards from "./components/ToDos/ToDoCards";
 
+const Main = styled.main`
+  display: flex;
+  flex-direction: row;
+`;
+
 function App() {
   return (
-    <main>
+    <Main>
       <Calendar />
       <ToDoCards />
-    </main>
+    </Main>
   );
 }
 
