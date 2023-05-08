cc28b6f21194c603aa3dae662abd7b79f2c3e7a5
jasmine0714 <woooooooow22@gmail.com>
2023-05-08 12:31:38 +0900

    ✨ state 끌어올리기

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Calendar from "./components/Calendar/Table";
 import ToDoCards from "./components/ToDos/ToDoCards";
+import { useDispatch, useSelector } from "react-redux";
+import { addMemo } from "./store/memoStore";
 
 const Main = styled.main`
   display: flex;
   flex-direction: row;
 `;
-
+type MemoProp = {
+  id: string;
+  text: string;
+};
 function App() {
+  const [memoList, setMemoList] = useState<string[]>([]);
+  const [memo, setMemo] = useState("");
+  console.log(memoList);
   return (
     <Main>
       <Calendar />
-      <ToDoCards />
+      <ToDoCards
+        memoList={memoList}
+        setMemoList={setMemoList}
+        memo={memo}
+        setMemo={setMemo}
+      />
     </Main>
   );
 }
--- src/components/ToDos/ToDoCards.tsx
+++ src/components/ToDos/ToDoCards.tsx
@@ -1,25 +1,13 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
-import { addMemo } from "../../store/memoStore";
 
-type MemoProp = {
-  id: string;
-  text: string;
-};
-
-function ToDoCards() {
-  const [memoList, setMemoList] = useState<string[]>([]);
-  const [memo, setMemo] = useState("");
-
+function ToDoCards({ memoList, setMemoList, memo, setMemo }: any) {
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    console.log(event.target.value);
     setMemo(event.target.value);
   };
 
   const onClick = () => {
     setMemoList([memo, ...memoList]);
-    console.log(memoList);
   };
   return (
     <div>
