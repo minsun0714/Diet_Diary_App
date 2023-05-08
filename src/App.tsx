275ce60f21264af0b517d059f21a19da5a0d7575
jasmine0714 <woooooooow22@gmail.com>
2023-05-08 13:24:53 +0900

    🎨 currentDate, setCurrentDate state 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Calendar from "./components/Calendar/Table";
 import ToDoCards from "./components/ToDos/ToDoCards";
-import { useDispatch, useSelector } from "react-redux";
-import { addMemo } from "./store/memoStore";
 
 const Main = styled.main`
   display: flex;
@@ -13,18 +11,26 @@ type MemoProp = {
   id: string;
   text: string;
 };
+
 function App() {
   const [memoList, setMemoList] = useState<string[]>([]);
   const [memo, setMemo] = useState("");
-  console.log(memoList);
+  const [currentDate, setCurrentDate] = useState(0);
+  console.log("memo", memo);
+  console.log("지금 날짜", currentDate);
+  const handleAddMemo = () => {
+    console.log(memoList);
+  };
+
   return (
     <Main>
-      <Calendar />
+      <Calendar currentDate={currentDate} setCurrentDate={setCurrentDate} />
       <ToDoCards
         memoList={memoList}
         setMemoList={setMemoList}
         memo={memo}
         setMemo={setMemo}
+        handleAddMemo={handleAddMemo}
       />
     </Main>
   );
